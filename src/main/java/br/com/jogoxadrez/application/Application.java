81df536e4a84342760112935b5e610b1b1c11c37
fabricio22 <fanpir13@hotmail.com>
2019-09-14 19:13:00 -0300

    Criando Classe de posicao.

--- src/main/java/br/com/jogoxadrez/application/Application.java
+++ src/main/java/br/com/jogoxadrez/application/Application.java
@@ -1,10 +1,14 @@
 package br.com.jogoxadrez.application;
 
+import br.com.jogoxadrez.tabuleiro.Posicao;
+
 public class Application {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+	  Posicao posicao = new Posicao(3, 5);
+	  
+	  System.out.println(posicao);
 	}
 
 }
