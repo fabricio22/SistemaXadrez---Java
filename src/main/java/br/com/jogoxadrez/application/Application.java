7faddf52e6cd07c21c1716211545cc476eb00b64
fabricio22 <fanpir13@hotmail.com>
2019-09-18 20:36:49 -0300

    Adicionando tratativa da exceções lançadas.

--- src/main/java/br/com/jogoxadrez/application/Application.java
+++ src/main/java/br/com/jogoxadrez/application/Application.java
@@ -1,7 +1,9 @@
 package br.com.jogoxadrez.application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import br.com.jogoxadrez.xadrez.ExcecaoXadrez;
 import br.com.jogoxadrez.xadrez.PartidaXadrez;
 import br.com.jogoxadrez.xadrez.PecaXadrez;
 import br.com.jogoxadrez.xadrez.PosicaoXadrez;
@@ -14,16 +16,25 @@ public class Application {
 		PartidaXadrez partidaXadrez = new PartidaXadrez();
 
 		while (true) {
-			UI.exibirTabulerio(partidaXadrez.getPecas());
-			System.out.println();
-			System.out.print("Digite a posicao da peca que voce deseja mover: ");
-			PosicaoXadrez origem = UI.leituraPosicaoPecaXadrez(sc);
-			
-			System.out.println();
-			System.out.print("Digite a posição de destino: ");
-			PosicaoXadrez destino = UI.leituraPosicaoPecaXadrez(sc);
+			try {
+				UI.limpaTela();
+				UI.exibirTabulerio(partidaXadrez.getPecas());
+				System.out.println();
+				System.out.print("Digite a posicao da peca que voce deseja mover: ");
+				PosicaoXadrez origem = UI.leituraPosicaoPecaXadrez(sc);
 
-			PecaXadrez pecaCapturada = partidaXadrez.executaMudancaPeca(origem, destino);
+				System.out.println();
+				System.out.print("Digite a posição de destino: ");
+				PosicaoXadrez destino = UI.leituraPosicaoPecaXadrez(sc);
+
+				PecaXadrez pecaCapturada = partidaXadrez.executaMudancaPeca(origem, destino);
+			} catch (ExcecaoXadrez e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 
